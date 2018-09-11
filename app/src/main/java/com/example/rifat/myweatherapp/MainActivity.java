6700f9a617cf38aa2340d84fa1ad90d887bfbdb6
Rajoan Rahman Rifat <43123017+RajoanRahman@users.noreply.github.com>
2018-09-11 11:45:03 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/rifat/myweatherapp/MainActivity.java
+++ app/src/main/java/com/example/rifat/myweatherapp/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        city = "San Francisco,US"; //default
+        city = "Dhaka,BD"; //default
 
         cityName = (TextView) findViewById(R.id.cityNametxt);
         iconView = (ImageView) findViewById(R.id.image_viewId);
